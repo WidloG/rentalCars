89b4f47c0be59ee6aa437734cbb997eb2ced8241
WidloG <notatkiwidlo@gmail.com>
2023-01-11 20:49:00 +0100

    PERFECT

--- src/main/java/database/Cars.java
+++ src/main/java/database/Cars.java
@@ -3,26 +3,36 @@ package database;
 
 import jakarta.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "Cars")
 
 public class Cars {
+    @Column(name = "carsId")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Basic
+    @Column(name = "Brand")
     private String brand;
+    @Basic
+    @Column(name = "Model")
     private String model;
+    @Basic
+    @Column(name = "Power")
     private double power;
+    @Basic
+    @Column(name = "Price")
     private double price;
 
-    @OneToMany(mappedBy = "carId")
+    @OneToMany(mappedBy = "cars")
     private Set<Rents> Rents = new HashSet<>(0);
 
     public Cars() {}
 
-    @Column(name = "Id")
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+
 
     public int getId() {
         return id;
@@ -31,8 +41,6 @@ public class Cars {
         this.id = id;
     }
 
-    @Basic
-    @Column(name = "Brand")
     public String getBrand() {
         return brand;
     }
@@ -40,8 +48,6 @@ public class Cars {
         this.brand = brand;
     }
 
-    @Basic
-    @Column(name = "Model")
     public String getModel() {
         return model;
     }
@@ -49,8 +55,6 @@ public class Cars {
         this.model = model;
     }
 
-    @Basic
-    @Column(name = "Power")
     public double getPower() {
         return power;
     }
@@ -58,12 +62,23 @@ public class Cars {
         this.power = power;
     }
 
-    @Basic
-    @Column(name = "Price")
     public double getPrice() {
         return price;
     }
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cars cars = (Cars) o;
+        return id == cars.id && Double.compare(cars.power, power) == 0 && Double.compare(cars.price, price) == 0 && brand.equals(cars.brand) && model.equals(cars.model) && Rents.equals(cars.Rents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, brand, model, power, price, Rents);
     }
 }
--- src/main/java/database/Rents.java
+++ src/main/java/database/Rents.java
@@ -2,40 +2,43 @@ package database;
 
 import jakarta.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @Table(name = "Rents")
 
 public class Rents {
-
+    @Column(name = "Id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Basic
+    @Column(name = "dateOfRent")
     private Date dateOfRent;
+    @Basic
+    @Column(name = "dateOfReturn")
     private Date dateOfReturn;
+    @Basic
+    @Column(name = "rentPrice")
     private double rentPrice;
-    @ManyToOne
-    @JoinColumn(name="carID")
-    private Cars carId;
-    @ManyToOne
-    @JoinColumn(name="clientID")
-    private Clients clientId;
 
     @ManyToOne
-    @JoinColumn(name = "cars_id")
+    @JoinColumn(name="carsId", nullable = false)
     private Cars cars;
+    @ManyToOne
+    @JoinColumn(name="clientsId", nullable = false)
+    private Clients clients;
 
-    public Cars getCars() {
-        return cars;
-    }
+    public Clients getClients() {return clients;}
 
-    public void setCars(Cars cars) {
-        this.cars = cars;
-    }
+    public void setClients(Clients clients) {this.clients = clients;}
+
+    public Cars getCars() {return cars;}
+
+    public void setCars(Cars cars) {this.cars = cars;}
 
     public Rents() {}
 
-    @Column(name = "Id")
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     public int getId() {
         return id;
     }
@@ -43,64 +46,47 @@ public class Rents {
         this.id = id;
     }
 
-    //@ManyToOne(cascade = CascadeType.ALL)
-    //@JoinColumn(name = "carID", insertable = false, updatable = false)
-    public Cars getCar() {
-        return carId;
-    }
-    public void setCar(Cars carId) {
-        this.carId = carId;
-    }
-
-    //@ManyToOne(cascade = CascadeType.ALL)
-    //@JoinColumn(name = "clientID")
-    public Clients getClient() {
-        return clientId;
-    }
-
-    public void setClient(Clients clientId) {
-        this.clientId = clientId;
-    }
-
-    @Basic
-    @Column(name = "RentDate")
     public Date getDateOfRent() {
         return dateOfRent;
     }
-
     public void setDateOfRent(Date dateOfRent) {
         this.dateOfRent = dateOfRent;
     }
 
-    @Basic
-    @Column(name = "ReturnDate")
-    public Date getDateOfReturn() {
-        return dateOfReturn;
-    }
-
+    public Date getDateOfReturn() {return dateOfReturn;}
     public void setDateOfReturn(Date dateOfReturn) {
         this.dateOfReturn = dateOfReturn;
     }
 
-    @Basic
-    @Column(name = "Price")
     public double getRentPrice() {
         return rentPrice;
     }
-
     public void setRentPrice(double rentPrice) {
         this.rentPrice = rentPrice;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rents rents = (Rents) o;
+        return id == rents.id && Double.compare(rents.rentPrice, rentPrice) == 0 && dateOfRent.equals(rents.dateOfRent) && dateOfReturn.equals(rents.dateOfReturn) && cars.equals(rents.cars) && clients.equals(rents.clients);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, dateOfRent, dateOfReturn, rentPrice, cars, clients);
+    }
 
     @Override
     public String toString() {
         return "Rents{" +
                 "id=" + id +
-                ", client= " + clientId +
-                ", car= " + carId +
+                ", client= " + clients.getId() +
+                ", car= " + cars.getId() +
                 ", dateOfRental=" + dateOfRent +
                 ", dateOfReturn=" + dateOfReturn +
                 '}';
     }
+
 }
